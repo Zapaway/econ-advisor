5ad37960a2ae0111d94da3ccc107a249f1c6ca0d
JawandS <jawandsingh@gmail.com>
2024-03-23 23:35:32 -0400

    add larger ticker list

---
+++ backend/res/analyer.py
@@ -0,0 +1,36 @@
+# analyze the tickers and classify into short/mid/long terms and low/medium/high risk
+
+import json 
+import yfinance as yf
+
+ticker_info = {}
+
+with open('tickers.txt') as infile:
+    tickers = infile.read().splitlines()
+    for ticker in tickers:
+        tickerInfo = yf.Ticker(ticker).info
+        try:
+            beta = tickerInfo['beta']
+            if beta < 1:
+                risk = 'low'
+            elif beta < 1.3:
+                risk = 'medium'
+            else:
+                risk = 'high'
+            earnings = tickerInfo['trailingPE']
+            if earnings < 15:
+                term = 'short'
+            elif earnings < 25:
+                term = 'mid'
+            else:
+                term = 'long'
+            ticker_info[ticker] = {
+                'name': tickerInfo['longName'],
+                'ideal_ris': [risk],
+                'ideal_timeframes': [term]
+            }
+        except Exception as e:
+            print(f"Error: {e}")
+
+with open('all_stocks.json', 'w') as outfile:
+    json.dump(ticker_info, outfile, indent=4)
