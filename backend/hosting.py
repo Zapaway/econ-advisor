df2d0309360d9740452d46552a3ad93f66de0ae1
JawandS <jawandsingh@gmail.com>
2024-03-24 08:12:05 -0400

    work on methods

--- backend/hosting.py
+++ backend/hosting.py
@@ -1,8 +1,24 @@
 from flask import Flask
 from flask import request
 import json
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+
+def getNewsData(ticker):
+	tickerNewsPage = f'https://finance.yahoo.com/quote/{ticker}/news'
+	req = Request(tickerNewsPage, headers={'User-Agent': 'Mozilla/5.0'})
+	page = urlopen(req)
+	tickerHtml = page.read().decode("utf-8")
+	html_data = BeautifulSoup(tickerHtml, 'html.parser')
+
+	# Get the news articles
+	articles = html_data.find_all('li', class_='stream-item  svelte-7rcxn')
+	for article in articles:
+		article_link = article.find('a')['href']
+		article_title = article.find('span').text
+		print(f"Title: {article_title} Link: {article_link}")
 
 @app.route('/getTickers')
 def getTickers():
@@ -16,4 +32,5 @@ def getTickers():
 			valid_tickers.append(ticker)
 	return valid_tickers
 
-app.run(host="0.0.0.0", debug = True)
+# app.run(host="0.0.0.0", debug = True)
+print(getNewsData('QQQ'))
